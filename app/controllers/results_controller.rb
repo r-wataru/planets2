c4bd17a86728ce7a8bda5bd405d99deed5b2cd34
Wataru Sato <wataru@rubyquitous.co.jp>
2014-10-06 12:33:55 +0900

    投手成績表示

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -2,8 +2,12 @@ class ResultsController < ApplicationController
   def index
     @season = Season.find_by(use: true)
     regulation_number = @season.games.count * 1.4
+    regulation_pitcher_number = @season.games.count * 1
     @results = @season.individual_total_results.
       where("#{regulation_number} < plate_appearances")
+    @pitcher_results = @season.pitcher_total_results.
+      where("#{regulation_pitcher_number} < pitching_number")
+    # 野手成績
     if params[:change].present?
       if params[:change] == "game_count"
         @results = @results.sort{|a, b| b.set_games.count <=> a.set_games.count }
@@ -16,6 +20,15 @@ class ResultsController < ApplicationController
       end
     else
       @results = @results.order("batting_average DESC")
+    end
+
+    # 投手成績
+    if params[:p_change].present?
+      if params[:p_change] == "game_count"
+        @pitcher_results = @pitcher_results.sort{|a, b| b.set_games.count <=> a.set_games.count }
+      else
+        @pitcher_results = @pitcher_results.order("#{params[:p_change]} DESC")
+      end
     end
   end
 end
--- app/models/season.rb
+++ app/models/season.rb
@@ -21,7 +21,8 @@ require 'nkf'
 class Season < ActiveRecord::Base
   has_many :games
   has_many :individual_total_results
-  
+  has_many :pitcher_total_results
+
   validates :year, presence: true, uniqueness: true
   validates :name, presence: true
 
